546d4fee536ad29afd4e5f33f44c9934bf35253a
Onis2254 <muhydeenyusuf54@gmail.com>
2026-08-31 10:28:17 +0100

    fix(ci): resolve failing checks for #106

--- tests/components/proofs/interval-config-step.test.tsx
+++ tests/components/proofs/interval-config-step.test.tsx
@@ -1,1 +1,7 @@
-test('renders', ()=> {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import IntervalConfigStep from '../../components/proofs/IntervalConfigStep';
+
+test('renders', () => {
+  render(<IntervalConfigStep />);
+});
